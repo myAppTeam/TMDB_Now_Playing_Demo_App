c83b576f64cd9de873669d63334e42a339a976f5
Sachin Ambegave <sachinbalajiambegave@gmail.com>
2021-12-04 10:45:06 +0530

    Initial Commit

---
+++ TMDB_Now_Playing_Demo_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDB_Now_Playing_Demo_App
+//
+//  Created by Sachin Ambegave on 04/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
